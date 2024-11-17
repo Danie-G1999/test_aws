015a1551cfd4c1eefa126c33cbde2cc7396ab4d9
Juan Daniel <jg585480@gmail.com>
2024-11-16 19:51:30 -0500

    ajustes de configuracion en backend

--- backend/db/db.js
+++ backend/db/db.js
@@ -1,12 +1,15 @@
 const { Pool } = require('pg');
 
-// Configuración de la conexión
+// Configuración de la conexión utilizando variables de entorno
 const pool = new Pool({
-    host: 'localhost', // Dirección del servidor PostgreSQL
-    user: 'postgres', // Usuario de PostgreSQL
-    password: '1234', // Contraseña del usuario
-    database: 'postgres', // Nombre de la base de datos
-    port: 5432, // Puerto de PostgreSQL (por defecto es 5432)
+    host: process.env.DB_HOST, // Dirección del servidor PostgreSQL (desde RDS)
+    user: process.env.DB_USER, // Usuario de PostgreSQL (de RDS)
+    password: process.env.DB_PASSWORD, // Contraseña del usuario (de RDS)
+    database: process.env.DB_NAME, // Nombre de la base de datos (de RDS)
+    port: process.env.DB_PORT || 5432, // Puerto de PostgreSQL (usualmente 5432)
+    ssl: {
+        rejectUnauthorized: false, // Permite conexiones SSL
+    },
 });
 
 // Verificar la conexión
